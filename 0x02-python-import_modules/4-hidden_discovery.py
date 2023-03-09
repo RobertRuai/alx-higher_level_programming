359dafdd4ce6a5baafbb6862f83b09e80fd647c4
RobertRuai <robertruai4@gmail.com>
2023-03-09 09:26:26 -0800

    a program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    lines = dir(hidden_4)
+    for lines in hidden_4:
+        if line[0:2] != "__":
+            print("{}\n".format(line))
