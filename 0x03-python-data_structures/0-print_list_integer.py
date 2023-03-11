5f37cbe3bb1ba674ceb896ffb0f0132d489bc5a9
Robert Ruai Mawich <robertruai4@gmail.com>
2023-03-11 17:47:48 +0300

    a function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+
+    a = len(my_list)
+
+    for i in range(a):
+        print("{}".format(my_list[i]))
+    return my_list[i]
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This directory contains all python-data_structures project tasks
