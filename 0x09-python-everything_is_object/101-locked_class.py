d355b1c284bd2f9fc85ae37e6d3bd34775a69a14
RobertRuai <robertruai4@gmail.com>
2023-04-04 17:48:16 +0300

    a class LockedClass with no class or object attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-class LockedClass(object):
-    __slots__ = 'first_name'
+"""class LockedClass file"""
+
+
+class LockedClass():
+    """a class LockedClass with no class or object attribute"""
+    __slots__ = "first_name"
