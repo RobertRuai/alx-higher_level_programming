d598d7700210bae436fda487724c75a73d68153b
RobertRuai <robertruai4@gmail.com>
2023-04-13 02:13:22 +0300

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Student module """
+
+
+class Student():
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        n = {}
+        if attrs is not None:
+            for i, j in self.__dict__.items():
+                if i in attrs:
+                    n[i] = self.__dict__[i]
+            return n
+        return self.__dict__
+            
