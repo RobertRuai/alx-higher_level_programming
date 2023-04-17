2ece1c408fe6f9b7dc35de6f3e507aa61671da07
RobertRuai <robertruai4@gmail.com>
2023-04-18 00:04:20 +0300

    minor edits

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,23 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ base class"""
+     """This class inherits from Base and defines a Rectangle object.
+    Attributes:
+        __width (int): the width of the rectangle.
+        __height (int): the height of the rectangle.
+        __x (int): the horizontal (x) padding of the rectangle.
+        __y (int): the vertical (y) padding of the rectangle.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ init function  """
+        """Initializes the default attributes of the Base object.
+        Args:
+            width (int): the wanted width of the rectangle.
+            height (int): the wanted height of the rectangle.
+            x (int): the wanted horizontal (x) padding of the rectangle.
+            y (int): the wanted vertical (y) padding of the rectangle.
+            id (int): the wanted identifier of the Base object.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
