1632c565e7d551e3bfaaf5727abb6b3563a09f32
RobertRuai <robertruai4@gmail.com>
2023-04-18 15:29:21 +0300

    attempted the advanced tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """ base class module"""
 import json
 import os
+import csv
+import turtle
 
 
 class Base:
@@ -64,3 +66,76 @@ class Base:
         for i, j in enumerate(n_list):
             a_list.append(cls.create(n_list[i]))
         return n_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serializes the CSV string representation to file"""
+        fields = []
+        with open(cls.__name__ + ".csv", 'w') as f:
+            if list_objs is None or len(list_objs) <= 0:
+                f.write('[]')
+            else:
+                if cls.__name__ is "Rectangle":
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ is "Square":
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Deserializes the CSV string representation
+        of list_objs from a file.
+        """
+        fields = []
+        try:
+            with open(cls.__name__ + ".csv", 'r') as f:
+                if cls.__name__ is "Rectangle":
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ is "Square":
+                    fields = ['id', 'size', 'x', 'y']
+                reader = csv.DictReader(f, fieldnames=fields)
+                dcts = [dict([k, int(v)] for k, v in l.items())
+                        for l in reader]
+                return [cls.create(**dct) for dct in dcts]
+
+        except IOError:
+            return []
+
+    def draw(list_rectangles, list_squares):
+        """Opens a window and draws all the Rectangles and Squares.
+        Args:
+            list_rectangles (list): a list of rectangle instances.
+            list_squares (list): a list of square instances.
+        """
+        t = turtle.Turtle()
+        t.screen.bgcolor('#000000')
+        t.shape('turtle')
+        t.color('#ffffff')
+        t.penup()
+        t.goto(-200, 200)
+        for rect in list_rectangles:
+            t.goto(t.xcor() + (rect.width + 20), t.ycor() - (rect.height + 20))
+            t.up()
+            t.down()
+            for i in range(2):
+                t.forward(rect.width)
+                t.left(90)
+                t.forward(rect.height)
+                t.left(90)
+            t.penup()
+
+        t.goto(-200, -20)
+        for squ in list_squares:
+            t.goto(t.xcor() + (squ.width + 20), t.ycor() - (squ.height + 20))
+            t.up()
+            t.down()
+            for i in range(2):
+                t.forward(squ.width)
+                t.left(90)
+                t.forward(squ.height)
+                t.left(90)
+            t.penup()
+        t.Screen().exitonclick()
