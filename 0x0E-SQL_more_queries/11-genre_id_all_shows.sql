865e3e1a64a6b2afa3ce87a67ad084ae672c8b1c
RobertRuai <robertruai4@gmail.com>
2023-05-17 15:21:35 -0700

    a script that lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows
+
+SELECT t.title, g.genre_id
+	FROM tv_shows t
+LEFT OUTER JOIN tv_show_genres g
+	on t.id = g.show_id
+ORDER BY t.title, g.genre_id ASC;
