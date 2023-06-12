f10ab80c111b56ff26e0671ba747ed3d3ccf417b
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-12 23:55:23 +0000

    a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const ag = process.argv;
 if (ag.length <= 3) {
   console.log(0);
 } else {
-  const sd = ag.sort(function(a, b) { return b - a; })[3];
+  const sd = ag.sort(function (a, b) { return b - a; })[3];
   console.log(sd);
 }
