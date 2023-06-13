d4a9fd788762fc8a6346f8926077e17bc68910af
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-13 22:42:17 +0300

    instance method called charPrint(c) that prints the rectangle using the character c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareA = require('./5-square.js');
+
+class Square extends SquareA {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+}
+
+module.exports = Square;
