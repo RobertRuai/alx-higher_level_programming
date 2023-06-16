f3060fc7d2f45dbd00a230525f30c1a11a53b4f3
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-16 08:42:27 -0700

    script that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists all states from the database starting with N"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE '{:s}' OR '1'= '1' ORDER BY states.id ASC;"
+        .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if sys.argv[4] in row:
+            print(row)
+    cur.close()
+    conn.close()
