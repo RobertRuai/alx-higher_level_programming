f55d47ed1f98003e7eba345478311daa19db766a
Robert Ruai Mawich <robertruai4@gmail.com>
2023-06-30 05:29:59 -0700

    chmod edit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays the body of the response from POST request
-curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
