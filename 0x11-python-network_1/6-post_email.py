e652688b5b4059f4a7dd4c5f5ca65d15da2de37a
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-01 09:06:44 -0700

    6. POST an email #1 using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" sends POST request to URL with the email as a parameter"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {'email': sys.argv[2]}
+    res = requests.post(url, data=val)
+    print(res)
+
