8e848b4859662a52e2829473df5a7ae695eab381
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-02 04:02:36 -0700

    7. Error code #1 using requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     res = requests.get(sys.argv[1])
     if res.status_code >= 400:
         print("Error code: {}".format(res.status_code))
-    print(res.text)
+    else:
+        print(res.text)
