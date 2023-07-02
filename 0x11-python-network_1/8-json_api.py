57e11a8f0cd28c4470dbe272fb47ab1eb49f7238
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-02 04:49:06 -0700

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" sends POST request to URL with the letter as a parameter"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_use"
+    if len(sys.argv) >= 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+    res = requests.post(url, data={'q': q})
+    try:
+        jsn = res.json()
+        if jsn:
+            print("[{}] {}".format(jsn.get("id"), jsn.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
