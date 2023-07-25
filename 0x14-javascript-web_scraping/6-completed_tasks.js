307f7ff7b218d418a0e52268e551d4ee9966d688
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-25 22:39:48 +0300

    a script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,13 +5,15 @@ const request = require('request');
 
 request(url, function (error, response, body) {
   if (error) console.log(error);
-  res = {}
-  const jsn = JSON.parse(body);
-  for (let i = 0; i < jsn.length; i++) {
-    if (jsn[i].completed == true) {
-      if (!res[jsn[i].userId]) res[jsn[i].userId] = 1;
-      else res[jsn[i].userId] += 1;
+  else {
+    const res = {};
+    const jsn = JSON.parse(body);
+    for (let i = 0; i < jsn.length; i++) {
+      if (jsn[i].completed == true) {
+        if (!res[jsn[i].userId]) res[jsn[i].userId] = 1;
+        else res[jsn[i].userId] += 1;
+      }
     }
+    console.log(res);
   }
-  console.log(res);
 });
