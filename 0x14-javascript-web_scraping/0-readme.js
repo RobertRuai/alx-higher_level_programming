a7281bd669576b0cb38d8452b2db6791f2a80435
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-25 16:53:28 +0300

    a script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8',(error, data) => {
+fs.readFile(file, 'utf-8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
