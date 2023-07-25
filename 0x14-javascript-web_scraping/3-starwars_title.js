c25a58568a145378a4c52daf4e3fd5a895675209
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-25 18:46:20 +0300

    a script that prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ const request = require('request');
 request (url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
+  } else if (response.statusCode == '200') {
     console.log(JSON.parse(body).title);
   }
 });
